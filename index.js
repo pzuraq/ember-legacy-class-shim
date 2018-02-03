5d7bae3f54c0e214f875d32d1f462025258258f8
Christopher Garrett <me@pzuraq.com>
2018-02-03 10:00:26 -0800

    refactor(shim): Convert to a shim
    After exploring CoreObject more as we're trying to fix ES Classes in
    general, we've found a way to monkeypatch Ember instead of using a babel
    transform. This method is likely to be much more efficient and inline
    with future behavior of classes, so we're opting to switch to this
    strategy.

--- index.js
+++ index.js
@@ -1,110 +1,40 @@
 /* eslint-env node */
 'use strict';
 
-const path = require('path');
 const VersionChecker = require('ember-cli-version-checker');
 
-function requireTransform(transformName) {
-  let plugin = require(transformName);
-
-  plugin = plugin.__esModule ? plugin.default : plugin;
-
-  // adding `baseDir` ensures that broccoli-babel-transpiler does not
-  // issue a warning and opt out of caching
-  let pluginPath = require.resolve(`${transformName}/package`);
-  let pluginBaseDir = path.dirname(pluginPath);
-  plugin.baseDir = () => pluginBaseDir;
-  plugin._name = transformName;
-
-  return plugin;
-}
-
-function hasPlugin(plugins, name) {
-  for (let maybePlugin of plugins) {
-    let plugin = Array.isArray(maybePlugin) ? maybePlugin[0] : maybePlugin;
-    let pluginName = typeof plugin === 'string' ? plugin : plugin.name || plugin._name;
-
-    if (pluginName === name) {
-      return true;
-    }
-  }
-
-  return false;
-}
-
 module.exports = {
-  name: 'ember-decorators',
+  name: 'ember-legacy-class-shim',
 
   included(parent) {
     this._super.included.apply(this, arguments);
+    this._ensureFindHost();
 
-    const host = this._findHost();
-
-    host.project.ui.writeWarnLine(
-      'ember-legacy-class-transform: This transform has been deprecated, please switch to ember-legacy-class-shim'
-    );
+    let host = this._findHost()
 
     // Create a root level version checker for checking the Ember version later on
     const emberChecker = new VersionChecker(host).forEmber();
 
-    // Create a parent checker for checking the parent app/addons dependencies (for things like polyfills)
-    const babelChecker = new VersionChecker(parent).for('ember-cli-babel', 'npm');
-
-    if (!babelChecker.satisfies('^6.0.0-beta.1')) {
+    if (!emberChecker.isAbove('2.13.0')) {
+      host.import('vendor/ember-legacy-class-shim.js');
+    } else if (parent === host) {
+      // The shim is being used in an application, and no longer needed
       host.project.ui.writeWarnLine(
-        'ember-legacy-class-transform: You are using an unsupported ember-cli-babel version, ' +
-        'legacy class transform will not be included automatically'
+        'ember-legacy-class-shim: This shim is not needed for Ember >= 2.13.0'
       );
-
-      this._registeredWithParent = true;
-    } else if (emberChecker.isAbove('2.13.0')) {
-      if (parent === host) {
-        // The transform is being used in an application, and no longer needed
-        host.project.ui.writeWarnLine(
-          'ember-legacy-class-transform: this transform is not needed for Ember >= 2.13.0'
-        );
-      }
-
-
-      this._registeredWithParent = true;
     }
-
-    this.registerTransformWithParent(parent);
   },
 
-  /**
-   * Registers the legacy class transform with the parent addon or application.
-   *
-   * @param {Addon|EmberAddon|EmberApp} parent
-   */
-  registerTransformWithParent(parent) {
-    if (this._registeredWithParent) return;
-
-    const parentOptions = parent.options = parent.options || {};
-
-    // Create babel options if they do not exist
-    parentOptions.babel = parentOptions.babel || {};
-
-    // Create and pull off babel plugins
-    const plugins = parentOptions.babel.plugins = parentOptions.babel.plugins || [];
-
-    if (!hasPlugin(plugins, 'babel-plugin-ember-legacy-class-constructor')) {
-      const EmberLegacyClassConstructor = requireTransform('babel-plugin-ember-legacy-class-constructor');
-
-      plugins.push(EmberLegacyClassConstructor);
+  _ensureFindHost() {
+    if (!this._findHost) {
+      this._findHost = function findHostShim() {
+        let current = this;
+        let app;
+        do {
+          app = current.app || app;
+        } while (current.parent.parent && (current = current.parent));
+        return app;
+      };
     }
-
-    this._registeredWithParent = true;
-  },
-
-  _findHost() {
-    let current = this;
-    let app;
-
-    do {
-      app = current.app || app;
-    } while (current.parent.parent && (current = current.parent));
-
-    return app;
   }
 };
